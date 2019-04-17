a77d2ab73fb20751211b7011b7e25362ed0fed2e
易元平 <keco339@sina.com>
2019-04-17 17:18:23 +0800

    升级koa-json-url=expand中间件，支持重定向localhost

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const expandResources = require('./lib/expand');
 const pickResource = require('./lib/pick');
 
 
-function  expandResourceWithPick(resourceJSON, expand='', pick='') {
+function  expandResourceWithPick(resourceJSON, expand='', pick='',redirectLocalhostIP=null) {
     let expandKeys = [];
-    return expandResources(resourceJSON,expand,expandKeys).then(retJson=>{
+    return expandResources(resourceJSON,expand,expandKeys,redirectLocalhostIP).then(retJson=>{
         return pickResource(retJson, pick, expandKeys);
     });
 }
@@ -17,14 +17,16 @@ module.exports = {
     expandResource: expandResources,
     pickResource: pickResource,
     expandResourceWithPick: expandResourceWithPick,
-    routerPlugin: async (ctx, next) => {
-        let {expand='',pick=''} = ctx.query;
-        ctx.query = _.omit(ctx.query,['expand','pick']);
-        await next();
-        if(/^2/.test(`${ctx.status}`)){
-            let retJson = ctx.body || {};
-            retJson = await expandResourceWithPick(retJson,expand,pick);
-            ctx.body = retJson;
-        }
+    routerPlugin: (redirectLocalhostIP)=>{
+        return async (ctx, next) => {
+            let {expand='',pick=''} = ctx.query;
+            ctx.query = _.omit(ctx.query,['expand','pick']);
+            await next();
+            if(/^2/.test(`${ctx.status}`)){
+                let retJson = ctx.body || {};
+                retJson = await expandResourceWithPick(retJson,expand,pick,redirectLocalhostIP);
+                ctx.body = retJson;
+            }
+        };
     }
 };
